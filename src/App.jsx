c908a99148830db8623ff6ffb955b7f8ccd8607f
Polly Randle <randlepolly@gmail.com>
2024-09-17 14:56:00 +0100

    first file structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Hello name="World" />
+      <img className="logo" src={makersLogo}></img>
+    </>
+  );
+}
+
+export default App;
