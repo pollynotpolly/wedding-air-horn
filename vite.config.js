0c0344723a919ca67b58e16968868a9125a59d64
Polly Randle <randlepolly@gmail.com>
2024-09-19 11:41:39 +0100

    deployment fixes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: '/src/main.jsx', // Ensure the entry point is correctly defined
+      },
+      // Add external dependencies if needed
+      // external: ['some-external-module'],
+    },
+  },
   test: {
-    globals: true, // Allow us to use expect, describe etc. without importing in every file
-    environment: "jsdom", // We are testing a DOM environment, not Node
+    globals: true,
+    environment: "jsdom",
     setupFiles: "./tests/setup.js",
   },
 });
