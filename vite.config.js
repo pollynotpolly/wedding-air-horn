4e51f43002fc240ad4262c18fdd66128d692a507
Polly Randle <randlepolly@gmail.com>
2024-09-20 10:30:41 +0100

    absolute path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
   test: {
     globals: true,
     environment: "jsdom",
